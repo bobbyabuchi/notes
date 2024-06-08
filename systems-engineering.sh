4a128eea7c1517eab5e4804520f677635d2d04b6
Bobby ABUCHI <bobbyabuchi@gmail.com>
2024-06-08 14:20:21 +0100

    Update systems-engineering.sh

--- systems-engineering.sh
+++ systems-engineering.sh
@@ -1,4 +1,5 @@
-# Develope complex projects that meet objectives in an efficient way:
+Develope complex projects that meet objectives in an efficient way:
+
 Architecture Descriptions: 
   This is where the high-level structure of the software system is defined. It includes the identification of components, their relationships, and how they interact to fulfill the system’s requirements.
 Needs, Requirements, Constraints: 
@@ -7,3 +8,18 @@ Trade Studies:
   This involves evaluating different architectural solutions against the requirements and constraints to determine the most suitable approach. It’s a decision-making process that considers various factors like cost, performance, and risk.
 Verification and Validation: 
   Verification ensures that the software correctly implements a specific function according to its specifications. Validation checks that the software meets the needs and expectations of the stakeholders.
+
+Steps:
+- Project Objectives
+- Concept Formulation
+- Decompose and Define
+- Further Decomposition and Definition (Optional)
+- Design and Build Modules/Parts
+- Integrate
+- Further Integration (Optional)
+- Evaluation:
+  - is this what we meant to build?
+  - is this what the project wanted?
+
+System Review Cycles: 
+- Concept Review - System Requirements Review - System Design Review - Preliminary Design Review 
