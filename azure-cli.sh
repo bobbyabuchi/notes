33ab8c49cf9784a5a04c52197ff05ec0d05608f1
Bobby ABUCHI <bobbyabuchi@gmail.com>
2024-06-23 03:03:11 +0100

    Update azure-cli.sh

--- azure-cli.sh
+++ azure-cli.sh
@@ -3,7 +3,7 @@
 # create storage account
 RG=rg_sb_eastus_192200_1_171910405634
 location=eastus
-name=$cour
+name=courseraxe
 subscription="Company subscription" # when there is a space...
 # use variable
 az storage account create \
@@ -12,3 +12,21 @@ az storage account create \
   --location $location \
   --name courseraxe
 
+#.................................................
+az appservice plan create \
+--name blob-exercise-plan \
+--resource-group $RG \
+--sku FREE --location $location
+
+az webapp create \
+--name courseraxeApp \
+--plan blob-exercise-plan \
+--resource-group $RG
+
+CONNECTIONSTRING=$(az storage account show-connection-string \
+--name courseraxe \
+--output tsv)
+
+az webapp config appsettings set \
+--name courseraxeApp --resource-group $RG \
+--settings AzureStorageConfig:ConnectionString=$CONNECTIONSTRING AzureStorageConfig:FileContainerName=files
